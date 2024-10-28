d3e1569a225e3e2420b3ce3970ea6d128d708b63
Anawil Karavek <akaravek@hotmail.com>
2024-10-28 20:04:38 +0700

    optimized LocationSelector

--- src/components/overpass/LocationSelector.tsx
+++ src/components/overpass/LocationSelector.tsx
@@ -1,82 +1,54 @@
-'use client';
-
-import * as React from 'react';
-import { Check, ChevronsUpDown } from 'lucide-react';
-
-import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
 import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from '@/components/ui/command';
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover';
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { useStore } from '@/store';
+import React from 'react';
+import { FixedSizeList } from 'react-window';
 
 export function LocationSelector() {
   const { elements, currentIndex, access } = useStore();
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState<number>(currentIndex);
 
-  const placeholder = 'Select Location';
-
-  React.useEffect(() => {
-    setValue(currentIndex);
-  }, [currentIndex]);
+  const itemSize = 35;
+  const itemCount = elements ? elements.length : 0;
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[200px] justify-between"
-        >
-          {value + 1 || placeholder}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0" side="top">
-        <Command>
-          <CommandInput placeholder={placeholder} />
-          <CommandList>
-            <CommandEmpty>No location found.</CommandEmpty>
-            <CommandGroup>
-              {elements?.map((item, index) => (
-                <CommandItem
-                  key={item.id}
-                  value={index.toString()}
-                  onSelect={(currentValue) => {
-                    setValue(
-                      currentValue === value.toString()
-                        ? currentIndex
-                        : parseInt(currentValue)
-                    );
-                    setOpen(false);
-                    access(parseInt(currentValue));
-                  }}
-                >
-                  <Check
-                    className={cn(
-                      'mr-2 h-4 w-4',
-                      value === index ? 'opacity-100' : 'opacity-0'
-                    )}
-                  />
-                  {index + 1}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <Select
+      onValueChange={(currentValue) => {
+        access(parseInt(currentValue));
+      }}
+    >
+      <SelectTrigger className="w-[100px]">
+        <SelectValue placeholder={currentIndex + 1}>
+          {currentIndex + 1}
+        </SelectValue>
+      </SelectTrigger>
+      <SelectContent side="top" align="center">
+        <SelectGroup>
+          <SelectLabel>Select Location</SelectLabel>
+          <FixedSizeList
+            height={200}
+            itemCount={itemCount}
+            itemSize={itemSize}
+            width="100%"
+          >
+            {({ index, style }) => (
+              <SelectItem
+                key={elements![index].id}
+                value={index.toString()}
+                style={style}
+              >
+                {index + 1}
+              </SelectItem>
+            )}
+          </FixedSizeList>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
 }
