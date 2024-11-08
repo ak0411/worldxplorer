e75ca817cf74fd2367d8e908c46850cd33cdb94b
Anawil Karavek <akaravek@hotmail.com>
2024-11-09 00:29:17 +0700

    added loading

--- src/providers/map-provider.tsx
+++ src/providers/map-provider.tsx
@@ -1,6 +1,7 @@
 //Since the map will be laoded and displayed on client side
 'use client';
 
+import Loading from '@/components/shared/Loading';
 // Import necessary modules and functions from external libraries and our own project
 import { Libraries, useJsApiLoader } from '@react-google-maps/api';
 import { ReactNode } from 'react';
@@ -16,9 +17,14 @@ export function MapProvider({ children }: { children: ReactNode }) {
     libraries: libraries as Libraries,
   });
 
-  if (loadError) return <p>Encountered error while loading google maps</p>;
-
-  if (!scriptLoaded) return <p>Map Script is loading ...</p>;
+  if (loadError || !scriptLoaded) {
+    return (
+      <div className="flex size-full items-center justify-center">
+        {loadError && <p>Encountered error while loading google maps</p>}
+        {!scriptLoaded && <Loading />}
+      </div>
+    );
+  }
 
   // Return the children prop wrapped by this MapProvider component
   return children;
